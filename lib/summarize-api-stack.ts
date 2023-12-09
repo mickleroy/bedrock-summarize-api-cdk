4350300b1c8f10e7faee0041a4966cd4fd29de20
Michael Leroy <michael.leroy@energyaustralia.com.au>
2023-12-09 23:12:56 +1100

    working summary api

--- lib/summarize-api-stack.ts
+++ lib/summarize-api-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import path = require('path');
 
 export class SummarizeApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const apiGateway = new apigateway.RestApi(this, 'SummarizeApi', {
+      restApiName: 'Summarize API',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SummarizeApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaFunction = new NodejsFunction(this, 'SummarizeFunction', {
+      functionName: 'SummarizeFunction',
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '..', 'functions', 'summarize.ts'),
+    });
+
+    const apiGatewayIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+
+    const method = apiGateway.root.addMethod('POST', apiGatewayIntegration, {
+      authorizationType: apigateway.AuthorizationType.NONE
+    });
+
+    lambdaFunction.addToRolePolicy(
+      new PolicyStatement({
+        actions: ['bedrock:InvokeModel'],
+        resources: ['*'],
+      })
+    );
   }
 }
